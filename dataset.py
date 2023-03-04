1a6d1dac214818cb7fb29a0b8a2fde5817008d18
QHXRPG <105186795+QHXRPG@users.noreply.github.com>
2023-03-04 10:00:45 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -20,13 +20,13 @@ def imshow(img_name:str,img:np.ndarray):
     cv2.waitKey(1)
 
 class Dataset(Data.Dataset):
-    def __init__(self,path, batch_size,width,height):
+    def __init__(self,path, train_batch_size,val_batch_size,width,height):
         super(Dataset, self).__init__()
         self.path = path
-        self.batch_size = batch_size
+        self.train_batch_size = train_batch_size
+        self.val_batch_size = val_batch_size
         self.width = width
         self.height = height
-        self.img = torch.rand(0)
     def to_tensor(self,img):
         img = torch.tensor(img, dtype=torch.float32)
         img = img.permute(2, 0, 1).contiguous()
@@ -59,6 +59,13 @@ class Dataset(Data.Dataset):
         :return: (图片,类别,anchoe)
         """
         data_zip=()
+        label = []
+        anchor = []
+        images = torch.rand(0)
+        label_all = []
+        anchor_all = []
+        images_all = []
+        i=0
         for root, dirs, files in os.walk(self.path):
             break
         if 'train' not in dirs or 'val' not in dirs:
@@ -66,22 +73,47 @@ class Dataset(Data.Dataset):
         train_img_path = self.path +'/' +'train' + '/' +'images/'
         train_labels_path = self.path +'/' +'train' + '/' +'labels/'
         for image in os.listdir(train_img_path):
-            print(image)
             if image =='.DS_Store':
                 continue
             img = cv2.imread(train_img_path + image)
             img = self.resize_image(img,self.width,self.height)
             img = self.to_tensor(img)
+            images = torch.cat([images, img],axis=0)
             with open(train_labels_path + image[:-5] + '.txt',"r") as file:
                 lines= file.readlines()
-                data_zip = data_zip + ((img, tuple(lines[0])[0], tuple(lines[1:])),)
-        return data_zip
+            label.append(int(lines[0][:-1]))
+            anchor.append(lines[1:])
+        label = np.array(label, dtype=np.int32)
+        label = torch.tensor(label, dtype=torch.long)
+        for i in range(len(anchor)):
+            for j in range(len(anchor[i])):
+                anchor[i][j] = anchor[i][j].replace("\n","")
+                anchor[i][j] = list(map(int, anchor[i][j].split()))
+            anchor[i]=np.array(anchor[i], dtype=np.float32)
+            anchor[i]= torch.tensor(anchor[i], dtype=torch.float32)
+        for i in range(int(round(len(anchor) / self.train_batch_size, 0))):
+            if (i + 1) * self.train_batch_size < len(anchor):
+                anchor_all.append(anchor[i * self.train_batch_size: (i + 1) * self.train_batch_size])
+                images_all.append(images[i * self.train_batch_size: (i + 1) * self.train_batch_size])
+                label_all.append(label[i * self.train_batch_size: (i + 1) * self.train_batch_size])
+            else:
+                anchor_all.append(anchor[i * self.train_batch_size:])
+                images_all.append(images[i * self.train_batch_size:])
+                label_all.append(label[i * self.train_batch_size:])
+        return zip(images_all,label_all,anchor_all)
     def Loader_val(self):
         """
         加载验证集
         :return: (图片,类别,anchoe)
         """
         data_zip=()
+        label = []
+        anchor = []
+        images = torch.rand(0)
+        label_all = []
+        anchor_all = []
+        images_all = []
+        i=0
         for root, dirs, files in os.walk(self.path):
             break
         if 'train' not in dirs or 'val' not in dirs:
@@ -94,12 +126,41 @@ class Dataset(Data.Dataset):
             img = cv2.imread(val_img_path + image)
             img = self.resize_image(img,self.width,self.height)
             img = self.to_tensor(img)
+            images = torch.cat([images, img],axis=0)
             with open(val_labels_path + image[:-5] + '.txt',"r") as file:
                 lines= file.readlines()
-                data_zip = data_zip + ((img, tuple(lines[0])[0], tuple(lines[1:])),)
-        return data_zip
+            label.append(int(lines[0][:-1]))
+            anchor.append(lines[1:])
+        label = np.array(label, dtype=np.int32)
+        label = torch.tensor(label, dtype=torch.long)
+        for i in range(len(anchor)):
+            for j in range(len(anchor[i])):
+                anchor[i][j] = anchor[i][j].replace("\n","")
+                anchor[i][j] = list(map(int, anchor[i][j].split()))
+            anchor[i]=np.array(anchor[i], dtype=np.float32)
+            anchor[i]= torch.tensor(anchor[i], dtype=torch.float32)
+        for i in range(int(round(len(anchor) / self.val_batch_size, 0))):
+            if (i + 1) * self.val_batch_size < len(anchor):
+                anchor_all.append(anchor[i * self.val_batch_size: (i + 1) * self.val_batch_size])
+                images_all.append(images[i * self.val_batch_size: (i + 1) * self.val_batch_size])
+                label_all.append(label[i * self.val_batch_size: (i + 1) * self.val_batch_size])
+            else:
+                anchor_all.append(anchor[i * self.val_batch_size:])
+                images_all.append(images[i * self.val_batch_size:])
+                label_all.append(label[i * self.val_batch_size:])
+        label_all = np.array(label_all, dtype=np.int)
+        label_all = torch.tensor(label_all, dtype=torch.long)
+        return zip(images_all,label_all,anchor_all)
 
+# label = np.array(labels, dtype=np.int)
+# label = torch.tensor(labels, dtype=torch.long)
 
 if __name__ == "__main__":
-    data = Dataset("/Users/qiuhaoxuan/PycharmProjects/深度学习视觉实战/yolo-v3-spp/my_yolo_dataset",64,224,224)
+    import time
+    a1=time.time()
+    data = Dataset("/Users/qiuhaoxuan/PycharmProjects/深度学习视觉实战/yolo-v3-spp/my_yolo_dataset",64,20,224,224)
     data=data.Loader_train()
+    a2=time.time()-a1
+
+    for i,(x,y,z) in enumerate(data):
+        break
